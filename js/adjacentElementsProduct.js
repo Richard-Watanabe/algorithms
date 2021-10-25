ce45337b750d9d168aef30807a5f131a9ec9f57e
Richard Watanabe <shoryuwatanabe124@gmail.com>
2021-10-25 23:53:39 +0000

    Completed adjacentElementsProduct

---
+++ js/adjacentElementsProduct.js
@@ -0,0 +1,9 @@
+function adjacentElementsProduct(inputArray) {
+  let largestProduct = inputArray[0] * inputArray[1];
+  for (let i = 1; i < inputArray.length - 1; i++) {
+    largestProduct = Math.max(largestProduct, inputArray[i] * inputArray[i + 1]);
+  }
+  return largestProduct;
+}
+
+adjacentElementsProduct();
