bacc1ef6d18fd1431c992869f2e4365a8f52b54e
Richard Watanabe <shoryuwatanabe124@gmail.com>
2021-11-06 02:46:07 +0000

    Completed alternatingSums

--- js/alternatingSums.js
+++ js/alternatingSums.js
@@ -1,7 +1,14 @@
 function alternatingSums(a) {
   if (a.length === 1) return [...a, 0];
   if (a.length === 2) return a;
-  // const group = { a: [], b: [] };
+  const group = { a: [], b: [] };
+  a.forEach((weight, i) => {
+    i % 2 === 0 ? group.a.push(weight) : group.b.push(weight);
+  });
+  Object.keys(group).forEach(key => {
+    group[key] = group[key].reduce((acc, val) => acc + val);
+  });
+  return Object.values(group);
 }
 
 alternatingSums();
