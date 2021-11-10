a28b7c371b521898fa4ca00c2edddbaed53a18b6
Richard Watanabe <shoryuwatanabe124@gmail.com>
2021-11-10 04:40:04 +0000

    Completed areSimilar

--- js/areSimilar.js
+++ js/areSimilar.js
@@ -2,14 +2,16 @@ function areSimilar(a, b) {
   let are = true;
   let test = null;
   let test2 = null;
+  let swap = false;
   for (let i = 0; i < a.length; i++) {
     if (a[i] !== b[i]) {
       if (test === null || test2 === null) {
         test = a[i]; test2 = b[i];
       } else {
-        if (test !== b[i] || test2 !== a[i]) {
+        if (swap || test !== b[i] || test2 !== a[i]) {
           are = false;
         }
+        swap = true;
       }
     }
   }
