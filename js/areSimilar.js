eb4ba794b4ae62dab773a284f0f12e7bd85df0fc
Richard Watanabe <shoryuwatanabe124@gmail.com>
2021-11-10 04:38:42 +0000

    In progress, areSimilar

--- js/areSimilar.js
+++ js/areSimilar.js
@@ -1,9 +1,19 @@
-// function areSimilar(a, b) {
-//   for (let i = 0; i < a.length; i++) {
-//     if (a[i] !== b[i]) {
-//     }
-//   }
+function areSimilar(a, b) {
+  let are = true;
+  let test = null;
+  let test2 = null;
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) {
+      if (test === null || test2 === null) {
+        test = a[i]; test2 = b[i];
+      } else {
+        if (test !== b[i] || test2 !== a[i]) {
+          are = false;
+        }
+      }
+    }
+  }
+  return are;
+}
 
-// }
-
-// areSimilar();
+areSimilar();
