419314ed4a6e547c6f60123ba24385725b730e80
Richard Watanabe <shoryuwatanabe124@gmail.com>
2021-11-11 23:54:28 +0000

    Completed arrayChange

--- js/arrayChange.js
+++ js/arrayChange.js
@@ -1,8 +1,14 @@
-// function arrayChange(inputArray) {
-//   var moves = 0;
-//   for (var i = 1; i < inputArray.length; i++) {
-//     if (inputArray[i]) {
-//     }
-//   }
-//   return moves;
-// }
+function arrayChange(inputArray) {
+  let max = inputArray[0];
+  let count = 0;
+  for (let i = 1; i < inputArray.length; i++) {
+    if (inputArray[i] <= max) {
+      count += max - inputArray[i] + 1;
+      inputArray[i] = max + 1;
+    }
+    max = inputArray[i];
+  }
+  return count;
+}
+
+arrayChange();
