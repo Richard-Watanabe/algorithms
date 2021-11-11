21aa8a435a03c6e2140b81dca65ea22a3ef1f234
Richard Watanabe <shoryuwatanabe124@gmail.com>
2021-11-11 01:11:03 +0000

    In progress, arrayChange

---
+++ js/arrayChange.js
@@ -0,0 +1,8 @@
+// function arrayChange(inputArray) {
+//   var moves = 0;
+//   for (var i = 1; i < inputArray.length; i++) {
+//     if (inputArray[i]) {
+//     }
+//   }
+//   return moves;
+// }
