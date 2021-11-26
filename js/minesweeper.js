dbc4cf32d3e649ea10c82155e08632eb38086b93
Richard Watanabe <shoryuwatanabe124@gmail.com>
2021-11-26 01:33:33 +0000

    Completed minesweeper

--- js/minesweeper.js
+++ js/minesweeper.js
@@ -34,6 +34,16 @@ function minesweeper(matrix) {
           game[i][j]++;
         }
       }
+      if (matrix[i + 1] !== undefined) {
+        if (matrix[i + 1][j - 1]) {
+          game[i][j]++;
+        }
+      }
+      if (matrix[i + 1] !== undefined) {
+        if (matrix[i + 1][j + 1]) {
+          game[i][j]++;
+        }
+      }
     }
   }
   return game;
