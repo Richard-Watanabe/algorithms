cd2d42eddd267c8445fd96642cf7944e865b8f16
Richard Watanabe <shoryuwatanabe124@gmail.com>
2021-11-29 21:30:39 +0000

    Completed alphabeticShift

--- js/alphabeticShift.js
+++ js/alphabeticShift.js
@@ -1,7 +1,8 @@
 function solution(inputString) {
   var letters = inputString.split('');
   for (let i = 0; i < letters.length; i++) {
-    const n = letters[i].charCodeAt() - 'a'.charCodeAt();
+    let n = letters[i].charCodeAt() - 'a'.charCodeAt();
+    n = (n + 1) % 26;
     letters[i] = String.fromCharCode(n + 'a'.charCodeAt());
   }
   return letters.join('');
