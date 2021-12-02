d42c3f15361445542ffbf7037bbddaa31b6cb151
Richard Watanabe <shoryuwatanabe124@gmail.com>
2021-12-02 00:56:27 +0000

    In progresss, chessBoardCellColor

--- js/chessBoardCellColor.js
+++ js/chessBoardCellColor.js
@@ -2,7 +2,7 @@ function solution(cell1, cell2) {
   const letters = 'ABCDEFGH';
   cell1 = letters.indexOf(cell1[0]);
   cell2 = letters.indexOf(cell2[0]);
-  return cell1;
+  return cell1 % 2 === cell2 % 2;
 }
 
 solution();
